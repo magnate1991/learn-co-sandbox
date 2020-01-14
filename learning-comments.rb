3ae0af229139992549a1e5beb59183377a2dd405
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-14 12:54:40 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,6 @@
+puts "Mr.Tolmasov Welcome to the Ruby"
+# It is my first code in ruby 
+puts "Joy to the world"
+puts "All the boys and girls"
+puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
